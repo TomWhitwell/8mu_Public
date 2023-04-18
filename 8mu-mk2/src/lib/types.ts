e6733ab16c240b211948d3367df46528a318e942
Tom Armitage <tom@infovore.org>
2023-04-18 11:06:23 +0100

    Types now reflect 8mu configuration.

--- 8mu-mk2/src/lib/types.ts
+++ 8mu-mk2/src/lib/types.ts
@@ -4,16 +4,28 @@ export type Control = {
   val?: number;
 };
 
+export type Button = {
+  channel: number;
+  paramA: number;
+  paramB: number;
+  mode: number;
+}
+
 export type ControllerConfiguration = {
-  ledOn: boolean;
-  ledFlash: boolean;
+  faderBlink: boolean;
+  accelBlink: boolean;
   controllerFlip: boolean;
   midiThru: boolean;
   deviceId: number;
   i2cLeader: boolean;
   faderMin: number;
   faderMax: number;
+  pageNumber: number;
+  dxMode: boolean;
+  midiMode: boolean;
   firmwareVersion: string;
   usbControls: Control[];
   trsControls: Control[];
+  usbButtons: Button[];
+  trsButtons: Button[];
 };
