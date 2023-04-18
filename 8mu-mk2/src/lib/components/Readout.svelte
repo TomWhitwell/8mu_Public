083c259282d28516a1c146af3e9dc507fa073338
Tom Armitage <tom@infovore.org>
2023-04-18 16:14:47 +0100

    Update viewing mode, and add tips library.

---
+++ 8mu-mk2/src/lib/components/Readout.svelte
@@ -0,0 +1,57 @@
+<script lang='ts'>
+  import { configuration } from '$lib/stores'
+  import { randomTip } from '$lib/tips';
+</script>
+
+{#if $configuration}
+<p>
+  Fader blink 
+  {#if $configuration.faderBlink}
+  on
+  {:else if !$configuration.faderBlink}
+  off 
+  {/if}
+  | 
+   
+  Gesture blink
+  {#if $configuration.accelBlink}
+  on
+  {:else if !$configuration.accelBlink}
+  off
+  {/if}
+  | 
+   
+  Usb on
+  {#if $configuration.controllerFlip}
+  left
+  {:else if !$configuration.controllerFlip}
+  right 
+  {/if}
+  | 
+   
+  Midi thru
+  {#if $configuration.midiThru}
+  on
+  {:else if !$configuration.midiThru}
+  off 
+  {/if}
+  | 
+   
+  {#if $configuration.midiMode}
+  Midi Type B
+  {:else if !$configuration.midiMode}
+  Midi Type A
+  {/if}
+  |
+   
+  DX mode
+  {#if $configuration.dxMode}
+  on
+  {:else if !$configuration.dxMode}
+  off 
+  {/if}
+   
+  Tip: {randomTip()}
+   
+  </p>
+{/if}
--- 8mu-mk2/src/routes/Viewing.svelte
+++ 8mu-mk2/src/routes/Viewing.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Button from "$lib/components/Button.svelte";
   import Control from "$lib/components/Control.svelte";
+  import Readout from "$lib/components/Readout.svelte";
   import Subhead from "$lib/components/Subhead.svelte";
   import { Tab, TabList, TabPanel, Tabs } from "$lib/components/tabs";
 
@@ -8,6 +9,7 @@
   import { exportConfig } from "$lib/import_export";
   import { deviceForId } from "$lib/configuration";
   import { logger } from "$lib/logger";
+  import ControlButton from "$lib/components/ControlButton.svelte";
 
   const doExportConfig = () => {
     if ($configuration) {
@@ -24,9 +26,11 @@
   };
 
   $: device = $configuration ? deviceForId($configuration.deviceId) : null;
+
+  $: currentBank = $configuration ? $configuration.pageNumber+1 : "?"
 </script>
 
-<Subhead title="Current Configuration">
+<Subhead title="Bank {currentBank}: Current Configuration">
   <Button
     label="Export current config"
     icon="file-export"
@@ -39,8 +43,12 @@
   <TabList>
     <Tab>USB</Tab>
     <Tab>TRS Jack</Tab>
+    <Tab>USB Buttons</Tab> 
+    <Tab>TRS Buttons</Tab> 
   </TabList>
+
   <TabPanel>
+    {#if $configuration && $configuration.usbControls}
     <div id="controls">
       {#each $configuration.usbControls as control, index}
         {#if device && index < device.controlCount}
@@ -48,9 +56,11 @@
         {/if}
       {/each}
     </div>
+    {/if}
   </TabPanel>
 
   <TabPanel>
+    {#if $configuration && $configuration.trsControls}
     <div id="controls">
       {#each $configuration.trsControls as control, index}
         {#if device && index < device.controlCount}
@@ -59,8 +69,35 @@
       {/each}
     </div>
     <p>There is no realtime preview of the TRS outputs.</p>
+    {/if}
+  </TabPanel>
+
+  <TabPanel>
+    {#if $configuration && device?.buttonCount && $configuration.usbButtons}
+    <div id="controls">
+      {#each $configuration.usbButtons as button, index}
+        {#if device && index < device.buttonCount}
+          <ControlButton {button} {index} />
+        {/if}
+      {/each}
+    </div>
+    {/if}
+  </TabPanel>
+
+  <TabPanel>
+    {#if $configuration && device?.buttonCount && $configuration.trsButtons}
+    <div id="controls">
+      {#each $configuration.trsButtons as button, index}
+        {#if device && index < device.buttonCount}
+          <ControlButton {button} {index} />
+        {/if}
+      {/each}
+    </div>
+    {/if}
   </TabPanel>
 </Tabs>
+
+<Readout />
 
 <style>
   #controls {
